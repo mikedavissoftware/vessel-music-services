20f821fb8bd1bd250c8f015b401832379775d914
Michael Davis <micdavis93@gmail.com>
2023-08-04 14:57:02 -0500

    invisible nav menu fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,6 @@ import "./sass/main.scss"
 
 import Header from "./components/Header"
 import HomePage from "./components/home-page/HomePage"
-import DaggettControlPage from "./components/project-pages/daggett-control/DaggettControlPage"
-import BackyardBowlsPage from "./components//project-pages/backyard-bowls/BackyardBowlsPage"
-import SpiritualDirectionPage from "./components/project-pages/spiritual-direction/SpiritualDirectionPage"
 // import ContactPage from "./components/ContactPage"
 import Footer from "./components/Footer"
 
@@ -20,9 +17,6 @@ export default function App() {
 
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/daggett-control" element={<DaggettControlPage />} />
-        <Route path="/backyard-bowls" element={<BackyardBowlsPage />} />
-        <Route path="/spiritual-direction" element={<SpiritualDirectionPage />} />
         {/* <Route path="/contact" element={<ContactPage />} /> */}
       </Routes>
 
