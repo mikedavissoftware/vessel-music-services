a1419b6f662c586c2f19cbfa8267a5fe0e71281b
Michael Davis <micdavis93@gmail.com>
2023-09-27 13:34:34 -0600

    console log mode and api key

--- client/src/components/home-page/sections/AboutVesselSection.jsx
+++ client/src/components/home-page/sections/AboutVesselSection.jsx
@@ -3,6 +3,10 @@
 
 export default function AboutVesselSection() {
 
+  console.log(import.meta.env.MODE)
+
+  console.log(process.env.GOOGLE_API_KEY)
+
   fetch(`https://sheets.googleapis.com/v4/spreadsheets/1xEINcpCOSh1gvUiHjLd2X-2LBaN27yW8AIJnVkFdo6U/values/FeaturedWorks/?key=${(import.meta.env.MODE === "development") ? (import.meta.env.VITE_GOOGLE_API_KEY) : (process.env.GOOGLE_API_KEY)}`)
   .then(r => r.json())
   .then(data => {
